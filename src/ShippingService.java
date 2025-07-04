324545e515d3fe90704b49609ad523c93d983690
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-07-04 22:14:52 +0300

    Feat: implment ShippingService it takes list of shippalbe items and it extracts its name and weight calculating final total weight

---
+++ src/ShippingService.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class ShippingService {
+    public static void ship(List<IShippableItem> items) {
+        if (items.isEmpty()) return;
+
+        System.out.println("** Shipment notice **");
+        double totalWeight = 0;
+
+        for (IShippableItem item : items) {
+            System.out.println(item.getName() + "\t\t" + item.getWeight() + "kg");
+            totalWeight += item.getWeight();
+        }
+
+        System.out.printf("Total package weight: %.2fkg\n", totalWeight);
+    }
+}
